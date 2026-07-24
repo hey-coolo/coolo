71274375f2c472cdfce34c0b9df55e1ca152aee7
coolo <hey@coolo.co.nz>
2026-07-24 15:59:05 +1200

    fuck

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
-import './index';
 
 const rootElement = document.getElementById('root');
 
