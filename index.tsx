a1b7c6a34fbd02ddebcff431de2e065bb94b6bf4
coolo <hey@coolo.co.nz>
2026-07-24 15:33:58 +1200

    below again

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
-import { CartProvider } from './context/CartContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
