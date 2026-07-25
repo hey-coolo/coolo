0b125f1ec219ebe6ddecb1150bf2fe02272cc2f9
coolo <hey@coolo.co.nz>
2026-07-25 16:33:38 +1200

    HOME Broke?

--- App.tsx
+++ App.tsx
@@ -119,8 +119,6 @@ const App: React.FC = () => {
                         <Route path="/contact" element={<ContactPage />} />
                         <Route path="/join" element={<JoinPage />} />
                         <Route path="/unsubscribe" element={<UnsubscribePage />} />
-
-                        <Route path="/styleguide" element={<StyleguidePage />} />
                         
                         <Route path="*" element={<NotFoundPage />} />
                       </Routes>
